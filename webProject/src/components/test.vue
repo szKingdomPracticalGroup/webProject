c89a5bccec3d8bb4e09689f03812232f05d92fb2
czj <986399434@qq.com>
2020-06-20 23:15:20 +0800

    新建web_develop，以后可以web端在这里操作

---
+++ webProject/src/components/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    {{msg}}
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'HelloWorld!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
